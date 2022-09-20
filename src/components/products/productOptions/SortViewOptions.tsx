3541cd1ffa8cab59f9b90bd029ee5d4c4014d50f
Gustav Pernber <gustav.pernber@icomera.com>
2022-09-20 23:04:17 +0200

    view change fungerar

--- src/components/products/productOptions/SortViewOptions.tsx
+++ src/components/products/productOptions/SortViewOptions.tsx
@@ -1,4 +1,7 @@
 import { SortByOptions } from "@/utils/types";
+import  smallArticles from '@/assets/icons/smallArticles.svg'
+import  bigArticles from '@/assets/icons/bigArticles.svg'
+
 
 type SortViewOptionsProps = {
   sortBy: SortByOptions;
@@ -18,7 +21,7 @@ function SortViewOptions({
       <form >
         <label
           htmlFor="hs-select-label"
-          className="block text-sm font-medium mb-2 "
+          className="block text-sm font-medium ml-1"
         >
           Sortera
         </label>
@@ -27,7 +30,7 @@ function SortViewOptions({
             value={sortBy}
             onChange={(e) => setSortBy(e.target.value)}
             id="hs-select-label"
-            className="py-3 px-4 pr-9 block w-full border-gray-200 rounded-md text-sm bg-green-100 "
+            className="py-3 px-4 pr-9 block  h-12 w-full border-gray-200 rounded-md text-sm bg-green-100 "
         >
           <option value='apk'>APK</option>
           <option value='alc_desc'>Högst alkoholhalt</option>
@@ -37,18 +40,25 @@ function SortViewOptions({
     );
   };
 
-  const CompactToggle = () => {
+  const CompactToggle = ({isCompactProducts, setIsCompactProducts}: {isCompactProducts: boolean, setIsCompactProducts: Function} )=> {
     return (
-        <div>
-            hello btn
-        </div>
+      
+    <button type="button" 
+    onClick={()=>setIsCompactProducts(!isCompactProducts)}
+    className="py-3 px-4 py-3 px-4 h-12 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm ">
+      Byt vy:
+      <img src={isCompactProducts ? smallArticles : bigArticles} alt="" />
+    </button>
     );
   };
 
   return (
-    <div className="py-3 flex flex-row justify-between">
+    <div className="py-3 flex flex-row justify-between items-end">
       <Sort />
-      <CompactToggle />
+      <CompactToggle 
+        isCompactProducts={isCompactProducts}
+        setIsCompactProducts={setIsCompactProducts}
+      />
     </div>
   );
 }
