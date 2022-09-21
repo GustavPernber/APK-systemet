abc61774b7e04e4ed29a98c88eb6e1c18b680cc2
Gustav Pernber <gustav.pernber@icomera.com>
2022-09-21 17:13:35 +0200

    cypress and pipeline config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    BASE_URL_DEV: 'https://apk-systemet-dev.netlify.app',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
