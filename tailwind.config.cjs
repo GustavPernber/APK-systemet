28a406086ad9643e2b7ced13cfdb8ec80a4180fd
Gustav Pernber <gustav.pernber@icomera.com>
2022-09-21 22:56:56 +0200

    fixed pipeline

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",'node_modules/preline/dist/*.js',],
-  darkMode: false,
+  media: false,
   theme: {
     fontFamily:{
       'sans':['Roboto', ...defaultTheme.fontFamily.sans],
