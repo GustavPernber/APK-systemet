89fae77f3b3f35bf7de852941dd40d7433f3f23a
Gustav Pernber <gustav.pernber@icomera.com>
2022-09-21 11:35:21 +0200

    refactoring performance optimization

--- src/components/products/productOptions/SortViewOptions.tsx
+++ src/components/products/productOptions/SortViewOptions.tsx
@@ -48,10 +48,9 @@ function SortViewOptions({
     className="py-3 px-4 py-3 px-4 h-12 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm ">
       Byt vy:
 
-      <div className=" h-6 w-6">
-        {isCompactProducts ? 
-        <img src={smallArticles} alt="Small articles icon" /> : 
-        <img src={bigArticles} alt="Big articles icon" />}
+      <div className=" relative h-6 w-6">
+        <img src={smallArticles} alt="Small articles icon" className={`${!isCompactProducts && "opacity-0"} absolute`}/> 
+        <img src={bigArticles} alt="Big articles icon" className={`${isCompactProducts && "opacity-0"} absolute`}/>
       </div>
     </button>
     );
--- src/components/products/products/Product.tsx
+++ src/components/products/products/Product.tsx
@@ -23,31 +23,52 @@ const Product = (props:ProductProps) => {
     
     product.cat1 = product.cat1==="Cider%20%26%20blanddrycker" ? "Cider & blanddryck" : product.cat1
     
-    if (product.vintage === null && product.nameThin === null) {
-        product.nameAndVintage = "";
-    } else if (product.nameThin === null) {
-        product.nameAndVintage = product.vintage;
-    } else if (product.vintage === null) {
-        product.nameAndVintage = product.nameThin;
-    } else {
-        product.nameAndVintage = `${product.nameThin}, ${product.vintage}`;
+    
+    const formatNameVintage = () => {
+        let nameAndVintage
+        if (product.vintage === null && product.nameThin === null) {
+            nameAndVintage = "";
+        } else if (product.nameThin === null) {
+            nameAndVintage = product.vintage;
+        } else if (product.vintage === null) {
+            nameAndVintage = product.nameThin;
+        } else {
+            nameAndVintage = `${product.nameThin}, ${product.vintage}`;
+        }
+        return nameAndVintage
+    }
+    
+    const formatApk = () => {
+        return product.apk = parseFloat(product.apk).toPrecision(3);
     }
 
-    product.apk = parseFloat(product.apk).toPrecision(3);
+    const formatProductPath = () => {
+        const productUrlName = `${product.nameBold
+            .replace(/\s+/g, "-")
+            .toLowerCase()}-${props.product.productNumber}`;
     
+        const cat1NameURL = product.cat1==="Cider%20%26%20blanddrycker" ? "cider-blanddrycker" : product.cat1
     
-    if (product.price % 1 != 0) {
-        let numbers=product.price.toFixed(2).split('.')
-        // priceString="decimal"
-        product.priceString=`${numbers[0]}:${numbers[1]}`
-        // const decimalStr = num.toString().split('.')[1];
-    }else{
-        product.priceString=`${product.price.toFixed(0)}:-`
+        const productUrl = `https://www.systembolaget.se/produkt/${cat1NameURL}/${productUrlName}`;
+
+        return productUrl
     }
     
+    const formatPriceString= () => {
+        let priceString
+        if (product.price % 1 != 0) {
+            let numbers=product.price.toFixed(2).split('.')
+            // priceString="decimal"
+            priceString=`${numbers[0]}:${numbers[1]}`
+            // const decimalStr = num.toString().split('.')[1];
+        }else{
+            priceString=`${product.price.toFixed(0)}:-`
+        }
+        return priceString
+    }
 
     return (
-        <a href="" className={`${product.assortmentText === "Ordervaror" ? "bg-gray-200" : "bg-white"} grid w-full grid-rows-[auto auto auto] grid-cols-1 h-auto px-5 py-3 shadow rounded`} >
+        <a href={formatProductPath()} className={`${product.assortmentText === "Ordervaror" ? "bg-gray-200" : "bg-white"} grid w-full grid-rows-[auto auto auto] grid-cols-1 h-auto px-5 py-3 shadow rounded`} >
             
             <div className="  w-full flex flex-row justify-start h-auto  min-h-[8rem] ">
                 <div className="   h-24  w-16 ">
@@ -67,19 +88,19 @@ const Product = (props:ProductProps) => {
                         </p>
 
                         <h1 className=" font-normal">{product.nameBold}</h1>
-                        <h1 className=" font-serif font-normal text-gray-400">{product.nameAndVintage}</h1>
+                        <h1 className=" font-serif font-normal text-gray-400">{formatNameVintage()}</h1>
 
                     </div>
 
                     <div className="w-full flex flex-row justify-between items-center pb-3">
                         <div className="flex flex-row  flex-wrap
                           justify-start   gap-x-3 text-sm whitespace-nowrap">
-                            <p className=" font-semibold">APK: {product.apk}</p>
+                            <p className=" font-semibold">APK: {formatApk()}</p>
                             <p>{product.volume} ml</p>
                             <p>{product.alcPercentage} %</p>
 
                         </div>
-                        <p className=" font-bold text-sm self-end ">{product.priceString}</p>
+                        <p className=" font-bold text-sm self-end ">{formatPriceString()}</p>
                        
                     </div>
                 </div>
@@ -113,7 +134,8 @@ const Product = (props:ProductProps) => {
                 <span className=" bg-orange rounded-md w-full font-condensed  text-xs p-1 flex justify-center font-bold text-gray-800 uppercase tracking-wide ">Ordervara</span>
             </div>)}
 
-        </a>)
+        </a>
+    )
 }
 
 export default Product
