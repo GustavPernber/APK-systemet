0e23de7f8149e7f56abc2eeeac6bc3275b9739a8
Gustav Pernber <gustav.pernber@icomera.com>
2022-09-21 18:14:08 +0200

    test update

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   env: {
-    BASE_URL_DEV: 'https://apk-systemet-dev.netlify.app',
+    BASE_URL_DEV: 'http://localhost:8888',
   },
   e2e: {
     setupNodeEvents(on, config) {
