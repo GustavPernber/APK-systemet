d9e92920f82444fb76ded3cb7cdbd4ed9bba2f5a
Gustav Pernber <gustav.pernber@icomera.com>
2022-09-23 20:39:19 +0200

    styling update

--- src/components/products/options/filters/utils/Accordion.tsx
+++ src/components/products/options/filters/utils/Accordion.tsx
@@ -20,7 +20,7 @@ function Accordion({ contentComponent, title }: AccordionProps) {
           className="hs-accordion-toggle  group py-3 inline-flex items-center justify-between gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500"
           aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one"
         >
-          <h2 className={`font-normal ${ show && "font-semibold "} text-lg`}>
+          <h2 className={`font-normal ${ show && "font-semibold "} text-xl`}>
             {title}
           </h2>
           <svg
@@ -38,21 +38,6 @@ function Accordion({ contentComponent, title }: AccordionProps) {
               stroke-linecap="round"
             />
           </svg>
-          {/* <svg
-            className="hs-accordion-active:block hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-            />
-          </svg> */}
         </button>
         <div
           id="hs-basic-with-title-and-arrow-stretched-collapse-three"
