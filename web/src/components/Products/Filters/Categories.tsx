826c68ff70d0ac00d099f9acc060553c01631183
Gustav Pernber <gustav.pernber@icomera.com>
2022-10-21 11:18:02 +0200

    hover effect on cat1

--- web/src/components/Products/Filters/Categories.tsx
+++ web/src/components/Products/Filters/Categories.tsx
@@ -9,26 +9,24 @@ function Categories() {
     const {metadata} = useContext(AppContext)
 
     const CategoriesContent = () => {
+
+        const categoriesWithAll = [{value: "all"}, ...metadata.categories.cat1]
+
         return(
             <div className=' pb-3 grid place-items-start'>
                 <div className='border-[1px] bg-gray-300 border-gray-300 
                 
-                rounded-lg overflow-hidden flex flex-col gap-[1px] w-full
+                rounded-md overflow-hidden flex flex-col gap-[1px] w-full
                 '>
-                    <button 
-                    onClick={() => setCat1({value:"all"}) }
-                    className={`${cat1.value === "all" && "bg-green-400 text-white font-semibold"} 
-                    bg-white transition-colors text-sm py-3 pr-8 pl-4 w-full grid place-content-start md:text-sm`}>
-                        Visa alla
-                    </button>
-                    {metadata.categories.cat1.map((category) =>{
+
+                    {categoriesWithAll.map((category) =>{
                         return(
                             <button 
                             key={category.value}
                             onClick={() => setCat1({value: category.value}) }
-                            className={`${cat1.value === category.value && "bg-green-400 text-white font-semibold "} 
-                            bg-white transition-colors text-sm py-3 pr-8 pl-4 w-full grid place-content-start`}>
-                            {category.value}
+                            className={`${cat1.value === category.value ? "bg-green-400 text-white font-semibold " : "hover:bg-gray-200"} 
+                            bg-white transition duration-75 text-sm py-3 pr-8 pl-4 w-full grid place-content-start`}>
+                            {category.value === "all" ? "Visa alla" : category.value}
                             </button>
                         )
                     })}
