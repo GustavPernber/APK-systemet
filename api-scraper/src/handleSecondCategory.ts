4547a599eb7d4e41b3acac578fdbed8d5a4a9c00
Gustav Pernber <gustav.pernber@icomera.com>
2024-07-11 23:34:43 +0200

    Update scraper

--- api-scraper/src/handleSecondCategory.ts
+++ api-scraper/src/handleSecondCategory.ts
@@ -2,41 +2,38 @@ import config from "./config.json";
 import { Product } from "./utils/types";
 import axios from "axios";
 
-export default async function handleSecondCategory(cat1:any, cat2: any){
+export default async function handleSecondCategory(cat1: any, cat2: any) {
+  let maxPages = false;
+  let allProducts: Product[] = [];
 
-    let maxPages = false;
-    let allProducts: Product[] = []
+  for (let i = 1; !maxPages; i++) {
+    const url = `${
+      config.systembolaget_api_url
+    }page=${i}&categoryLevel1=${encodeURIComponent(
+      cat1.value
+    )}&categoryLevel2=${encodeURIComponent(cat2.value)}`;
+    const result = await axios({
+      url,
+      headers: {
+        ...config.headers,
+      },
+    });
 
-    for (let i = 1; !maxPages; i++) {
+    console.log("Fetched: ", allProducts.length);
 
-        const url = `${ config.systembolaget_api_url }page=${i}&categoryLevel1=${encodeURIComponent(cat1.value)}&categoryLevel2=${encodeURIComponent(cat2.value)}`
-        const result = await axios( 
-            {
-                url,
-                headers: {
-                    ...config.headers
-                }
-            },
-        )
+    allProducts = [...allProducts, ...result.data.products];
 
-        console.log('Fetched: ', allProducts.length)
-
-        allProducts = [...allProducts, ...result.data.products]
-        
-        if (result.data.products < 1) {
-            maxPages = true;
-        }
-
+    if (result.data.products < 1) {
+      maxPages = true;
     }
+  }
 
-    const products = new Set()
-    const filteredProducts = allProducts.filter(product => {
-        const duplicate = products.has(product.productId);
-        products.add(product.productId);
-        return !duplicate;
-    })
-    
-    return filteredProducts
-    
+  const products = new Set();
+  const filteredProducts = allProducts.filter((product) => {
+    const duplicate = products.has(product.productId);
+    products.add(product.productId);
+    return !duplicate;
+  });
+
+  return filteredProducts;
 }
-
