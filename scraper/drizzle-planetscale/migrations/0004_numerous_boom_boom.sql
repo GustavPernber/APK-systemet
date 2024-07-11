dfe3ec7b7ed2333753d639d059b6393ef1f94e55
Gustav Pernber <gustav.pernber@icomera.com>
2024-07-11 23:57:54 +0200

    Fix migrations

--- scraper/drizzle-planetscale/migrations/0004_numerous_boom_boom.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `products` MODIFY COLUMN `usage` text;--> statement-breakpoint
-ALTER TABLE `products` MODIFY COLUMN `taste` text;--> statement-breakpoint
-ALTER TABLE `products` ADD CONSTRAINT `products_productId_scrapedAt_unique` UNIQUE(`productId`,`scrapedAt`);
